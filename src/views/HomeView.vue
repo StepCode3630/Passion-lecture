b3ac178237d139778afd2ce8d7835d442891254f
StepCode3630 <196639646+StepCode3630@users.noreply.github.com>
2026-02-11 11:23:59 +0100

    feat(page accueil): création de la page d'accueil
    [20] [Wip]

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Découvrez l'art de la lecture</h1>
+    <p>Vous pouvez partager, ajoutez et commentez les ouvrages de la commu ETML!</p>
+  </div>
+</template>
+
+<script setup>
+// Ici tu peux importer des données, composants, API...
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 2rem;
+}
+</style>
