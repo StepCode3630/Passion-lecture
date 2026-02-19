841fcd809975efd48a6f0a4246a545c33202aa5c
CamilleJulieRais <rais.camille@gmail.com>
2026-02-19 21:57:01 +0100

    feat(creation de mockData)
    pour faire la tâche #4 Fonctionnalités requise: list d'ouvrage par catégorie, il faut au minimum une liste de livre

---
+++ src/data/mockData.js
@@ -0,0 +1,38 @@
+// src/data/mockData.js
+
+export const users = [
+  { id: 1, pseudo: "StepCode3630", role: "admin" },
+  { id: 2, pseudo: "Camille", role: "user" },
+  { id: 3, pseudo: "Gino", role: "user" },
+]
+
+export const books = [
+  {
+    id: 101,
+    title: "Les Misérables",
+    author: "Victor Hugo",
+    category: "Roman",
+    userId: 1,
+  },
+  {
+    id: 102,
+    title: "Naruto - Tome 1",
+    author: "Masashi Kishimoto",
+    category: "Manga",
+    userId: 2,
+  },
+  {
+    id: 103,
+    title: "One Piece - Tome 1",
+    author: "Eiichiro Oda",
+    category: "Manga",
+    userId: 3,
+  },
+  {
+    id: 104,
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    category: "Informatique",
+    userId: 1,
+  },
+]
