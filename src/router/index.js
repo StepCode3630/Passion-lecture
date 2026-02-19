321773f9eb816c4ef1d00eacdc13832387a182c3
CamilleJulieRais <rais.camille@gmail.com>
2026-02-19 22:01:20 +0100

    feat(mise en place des routes dans index.js)
    [20][Done]

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// src/router/index.js
+import { createRouter, createWebHistory } from "vue-router"
+
+import HomeView from "@/views/HomeView.vue"
+import BooksListView from "@/views/BooksListView.vue"
+import BookDetailView from "@/views/BookDetailView.vue"
+import UserDetailView from "@/views/UserDetailView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: "/", name: "home", component: HomeView },
+    { path: "/books", name: "books", component: BooksListView },
+
+    // pour détail ouvrage
+    { path: "/books/:id", name: "book-detail", component: BookDetailView, props: true },
+
+    // chemin pour détail user
+    { path: "/users/:id", name: "user-detail", component: UserDetailView, props: true },
+
+    // pour fallback
+    { path: "/:pathMatch(.*)*", redirect: "/" },
+  ],
 })
 
 export default router
