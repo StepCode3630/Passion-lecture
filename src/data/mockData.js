e473503d118d2340a3e2ed9a8c78014739fe30d2
CamilleJulieRais <rais.camille@gmail.com>
2026-02-22 18:04:27 +0100

    feat(données mock backend)
    [15][Done]

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,4 +1,13 @@
 // src/data/mockData.js
+// pas de vrai backend donc on simule des données avec des tableaux d'objets
+// les données sont organisées en deux tableaux : users et books
+// chaque objet dans ces tableaux représente un utilisateur ou un livre avec des propriétés spécifiques
+// les utilisateurs ont des propriétés id, pseudo et role
+// les livres ont des propriétés id, title, author, category et userId (qui fait référence à l'id de l'utilisateur qui a ajouté le livre)
+// ces données peuvent être utilisées pour afficher des listes d'utilisateurs et de livres, ainsi que pour afficher les détails d'un livre ou d'un utilisateur spécifique
+// en exportant ces tableaux, ils peuvent être importés et utilisés dans d'autres parties de l'application, comme les composants Vue ou les services de données
+
+// les données sont stockée en mémoire / simulé / utilisé coté frontend donc pas de persistance réelle, les données seront réinitialisées à chaque chargement de la page
 
 export const users = [
   { id: 1, pseudo: "StepCode3630", role: "admin" },
