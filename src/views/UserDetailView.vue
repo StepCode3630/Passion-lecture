19d8827fad31085ff55445a00e6ae8d4b21c829b
CamilleJulieRais <rais.camille@gmail.com>
2026-02-22 18:18:06 +0100

    feat(userDetailView)
    [15][WIP]

--- src/views/UserDetailView.vue
+++ src/views/UserDetailView.vue
@@ -2,6 +2,9 @@
   <section class="page" v-if="user">
     <h1>Détail de l'utilisateur</h1>
 
+    <!-- la page affiche pseudo, role, liste livre crée par cet utilisateur -->
+
+
     <p><strong>Pseudo :</strong> {{ user.pseudo }}</p>
     <p><strong>Rôle utilisateur:</strong> {{ user.role }}</p>
 
@@ -28,12 +31,23 @@ import { computed } from "vue"
 import { RouterLink } from "vue-router"
 import { books, users } from "@/data/mockData.js"
 
+
+// avec defineProps on récupère l'ID de l'utilisateur depuis les paramètres de l'URL
+//on cherche l'utilisateur correspondant dans la liste des utilisateurs
+//on affiche ses détails pseudo, rôle et la liste de ses livres.
+//Si l'utilisateur n'est pas trouvé on affiche un message d'erreur.
 const props = defineProps({
   id: { type: String, required: true },
 })
 
+// la const user utilise computed pour trouver l'utilisateur correspondant à l'ID dans les paramètres de l'URL
+// en cherchant dans la liste des utilisateurs. Si l'utilisateur n'est pas trouvé, user.value sera undefined et on affichera un message d'erreur.
 const user = computed(() => users.find((u) => String(u.id) === props.id))
 
+
+// ici on utilise computed pour créer une liste de livres qui appartiennent à l'utilisateur trouvé
+// Si user.value est undefined donc utilisateur non trouvé, on retourne une liste vide.
+// sinon on filtre la liste des livres pour ne garder que ceux dont le userId correspond à l'ID de l'utilisateur.
 const userBooks = computed(() => {
   if (!user.value) return []
   return books.filter((b) => b.userId === user.value.id)
