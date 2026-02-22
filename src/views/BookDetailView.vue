f9a0ca3b2e11f98a9fa1235aca45f805a8753ad3
CamilleJulieRais <rais.camille@gmail.com>
2026-02-22 18:14:17 +0100

    feat(booksDetailView)
    [10][Done]

--- src/views/BookDetailView.vue
+++ src/views/BookDetailView.vue
@@ -2,6 +2,9 @@
   <section class="page" v-if="book">
     <h1>Détail des Livres</h1>
 
+    <!-- cette page récupére l ID depuis l'URL , cherche le livre correspondant, affiche ses infos, affiche lien vers utilisateur -->
+
+
     <h2>{{ book.title }}</h2>
     <p><strong>Auteur :</strong> {{ book.author }}</p>
     <p><strong>Catégorie :</strong> {{ book.category }}</p>
@@ -27,12 +30,22 @@ import { computed } from "vue"
 import { RouterLink } from "vue-router"
 import { books, users } from "@/data/mockData.js"
 
+
+// ici on récupère l'ID du livre depuis les paramètres de l'URL grâce à defineProps
+// on cherche le livre correspondant dans notre liste de livres, et on affiche ses détails.
+// On affiche aussi le pseudo de l'utilisateur qui a posté le livre, en cherchant dans la liste des utilisateurs.
 const props = defineProps({
   id: { type: String, required: true },
 })
 
+
+// on utilise computed pour trouver le livre correspondant à l'ID dans les paramètres
+// pour trouver le pseudo de l'utilisateur qui a posté le livre.
+// Si le livre n'est pas trouvé on affiche un message d'erreur.
 const book = computed(() => books.find((b) => String(b.id) === props.id))
 
+// computed pour trouver le pseudo de l'utilisateur qui a posté le livre, ou afficher "Utilisateur inconnu" si l'utilisateur n'est pas trouvé
+// on cherche l'utilisateur dans la liste des utilisateurs en comparant son ID avec le userId du livre, et on retourne son pseudo si trouvé, sinon un message d'erreur.
 const userPseudo = computed(() => {
   if (!book.value) return ""
   const user = users.find((u) => u.id === book.value.userId)
