5e7b97ccb8111d16169c683e37f2c6ccdc640621
CamilleJulieRais <rais.camille@gmail.com>
2026-02-22 18:01:05 +0100

    feat(router navigation)
    [20][Done]

--- src/router/index.js
+++ src/router/index.js
@@ -9,16 +9,23 @@ import UserDetailView from "@/views/UserDetailView.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", name: "home", component: HomeView },
-    { path: "/books", name: "books", component: BooksListView },
+    { path: "/", name: "home",
+      component: HomeView },
+      // chemin pour liste des livres
+    { path: "/books", name: "books",
+      component: BooksListView },
 
     // pour détail ouvrage
-    { path: "/books/:id", name: "book-detail", component: BookDetailView, props: true },
+    // :id est un patamètre dynamique qui correspond à l'identifiant du livre
+    { path: "/books/:id", name: "book-detail",
+      component: BookDetailView, props: true },
 
     // chemin pour détail user
-    { path: "/users/:id", name: "user-detail", component: UserDetailView, props: true },
+    { path: "/users/:id", name: "user-detail",
+    component: UserDetailView, props: true },
 
     // pour fallback
+    // : pathMatch(.*)* est une syntaxe spéciale qui capture tous les chemins qui ne correspondent à aucune route définie
     { path: "/:pathMatch(.*)*", redirect: "/" },
   ],
 })
