25af53634a9b6064a3fce4081bc00aa94d76d583
CamilleJulieRais <rais.camille@gmail.com>
2026-02-24 17:24:06 +0100

    feat(EditBookView)
    [10][WIP]

---
+++ src/views/EditBookView.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="page" v-if="book">
+    <h1>Modifier un ouvrage</h1>
+
+    <!-- -->
+
+    <h2>{{ book.title }}</h2>
+    <p></p>
+    <p></p>
+
+    <p>
+      <strong>Posté par :</strong>
+    </p>
+  </section>
+</template>
+
+<script setup>
+import {  } from
+import {  } from
+import {  } from
+</script>
+
+<style scoped>
+.page {
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
