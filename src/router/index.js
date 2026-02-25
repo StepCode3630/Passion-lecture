1f038cdf7995f14ba0fe632ef4b4f43c64e7c32b
StepCode3630 <196639646+StepCode3630@users.noreply.github.com>
2026-02-25 08:18:10 +0100

    Revert "Merge pull request #12 from StepCode3630/Fonctionnalités-requise-list-d'ouvrage-par-catégorie-#4"
    This reverts commit df85433dbc7fd35119c673c27d711adab941b307, reversing
    changes made to fc1a3a2c2b3010717c354c11e5e64844f7572840.

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,9 @@
-// src/router/index.js
-import { createRouter, createWebHistory } from "vue-router"
-
-import HomeView from "@/views/HomeView.vue"
-import BooksListView from "@/views/BooksListView.vue"
-import BookDetailView from "@/views/BookDetailView.vue"
-import UserDetailView from "@/views/UserDetailView.vue"
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", name: "home",
-      component: HomeView },
-      // chemin pour liste des livres
-    { path: "/books", name: "books",
-      component: BooksListView },
-
-    // pour détail ouvrage
-    // :id est un patamètre dynamique qui correspond à l'identifiant du livre
-    { path: "/books/:id", name: "book-detail",
-      component: BookDetailView, props: true },
-
-    // chemin pour détail user
-    { path: "/users/:id", name: "user-detail",
-    component: UserDetailView, props: true },
-
-    // pour fallback
-    // : pathMatch(.*)* est une syntaxe spéciale qui capture tous les chemins qui ne correspondent à aucune route définie
-    { path: "/:pathMatch(.*)*", redirect: "/" },
     {
       path: '/',
       name: 'home',
