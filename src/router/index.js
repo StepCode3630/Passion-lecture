b5819af5ce6f165b7ab6bbbc052be4d0d0653b4d
StepCode3630 <196639646+StepCode3630@users.noreply.github.com>
2026-03-03 15:33:27 +0100

    feat: création de la page "mes livres"
    [70] [Done]

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BookDetailView from '../views/BookDetailView.vue'
+import BooksListView from '@/views/BooksListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +14,7 @@ const router = createRouter({
     {
       path: '/mes-livres',
       name: 'books-list',
-      component: HomeView,
+      component: BooksListView,
     },
     {
       path: '/decouvrir',
