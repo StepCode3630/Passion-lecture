6806e4faa919ef656598ebfb1fe874b331c7bc7c
Stepan <stepanpatricny@gmail.com>
2026-05-05 17:24:44 +0200

    feat(auth): route
    [20] [WIP]

--- back-end/start/routes.ts
+++ back-end/start/routes.ts
@@ -13,12 +13,33 @@ import AuthorsController from '#controllers/authors_controller'
 import CategoriesController from '#controllers/categories_controller'
 import CommentairesController from '#controllers/commentaires_controller'
 import UsersController from '#controllers/users_controller'
+import User from '#models/user'
+import { middleware } from '#start/kernel'
+import { validateHeaderValue } from 'http'
 
-// routes publique
-router.resource('books', BooksController).apiOnly()
-router.resource('authors', AuthorsController).apiOnly()
-router.resource('categories', CategoriesController).apiOnly()
-router.resource('commentaires', CommentairesController).apiOnly()
-
+router
+  .group(() => {
+    // routes publique
+    router.resource('books', BooksController).apiOnly()
+    router.resource('authors', AuthorsController).apiOnly()
+    router.resource('categories', CategoriesController).apiOnly()
+    router
+      .group(() => {
+        router.resource('commentaires', CommentairesController).apiOnly()
+      })
+      .prefix('books/:bookId')
+  })
+  .use(middleware('auth'))
 // routes protégées besoin d'auth
 router.resource('users', UsersController).apiOnly()
+
+router.post('/users/:id/tokens', async ({ params }) => {
+  const user = await User.findOrFail(params.id)
+  const token = await User.accessTokens.create(user)
+
+  return {
+    type: 'bearer',
+    value: token.value!.release(),
+  }
+})
+router.delete('/projects/:id')
