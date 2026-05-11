edac92829deaf83f197e8f92929bcfce24d4c9d7
CamilleJulieRais <rais.camille@gmail.com>
2026-05-11 13:36:00 +0200

    feat(migration) : correction des unique sans (), annee_publication doit etre en integer pour coller avec l ensemble du code, et on permet les table nullable pour eviter la creation des livre avant les auteur
    [10][DONE]

--- back-end/database/migrations/4_1777894086086_create_books_table.ts
+++ back-end/database/migrations/4_1777894086086_create_books_table.ts
@@ -7,12 +7,12 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable
 
-      table.string('titre').unique
+      table.string('titre').unique()
       table.string('image')
-      table.date('annee_publication')
+      table.integer('annee_publication')
       table.integer('nb_page')
 
-      table.string('lien_extrait').unique
+      table.string('lien_extrait').unique()
 
       table.string('resume')
       table.string('editeur')
@@ -27,7 +27,8 @@ export default class extends BaseSchema {
       table
         .integer('author_id')
         .unsigned()
-        .notNullable()
+        // temporairement nullable pour éviter les problèmes de création de livres avant les auteurs
+        .nullable()
         .references('id')
         .inTable('authors')
         .onDelete('CASCADE')
