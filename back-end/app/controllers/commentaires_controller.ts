3fd3cdba02320cfa58ba9d5cd66ef91112eaea9a
Stepan <stepanpatricny@gmail.com>
2026-05-12 15:20:36 +0200

    feat: maj controlleur crud avec auth commentaires
    [15] [Done]

--- back-end/app/controllers/commentaires_controller.ts
+++ back-end/app/controllers/commentaires_controller.ts
@@ -1,5 +1,9 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import Commentaire from '#models/commentaire'
+import Commentaire from '#models/comment'
+import { Bouncer } from '@adonisjs/bouncer'
+import CommentPolicy from '#policies/comment_policy'
+import { resolve } from 'path'
+import { messages } from '@vinejs/vine/defaults'
 
 export default class CommentairesController {
   /**
@@ -14,13 +18,12 @@ export default class CommentairesController {
    * Handle form submission for the create action
    */
   async store({ request, response }: HttpContext) {
-    const { message, etoile, userId, bookId } = request.all()
+    const { message, etoile, userId } = request.all()
 
     const data = {
       message,
       etoile,
       userId,
-      bookId,
     }
 
     return response.created(data)
@@ -34,25 +37,34 @@ export default class CommentairesController {
     return commentaire
   }
 
-  /**
+  /** A SUPPRIMER !!!!!!!!
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {
-    const data = request.all()
-    const commentaire = await Commentaire.findOrFail(params.id)
+  // async update({ params, request }: HttpContext) {
+  //   const data = request.all()
+  //   const commentaire = await Commentaire.findOrFail(params.id)
 
-    commentaire.merge(data)
-    await commentaire.save()
+  //   commentaire.merge(data)
+  //   await commentaire.save()
 
-    return commentaire
-  }
+  //   return commentaire
+  // }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {
-    const commentaire = await Commentaire.findOrFail(params.id)
-    await commentaire.delete()
-    return commentaire
+  async destroy({ params, response, bouncer }: HttpContext) {
+    const comment = await await Commentaire.query()
+      .where('id', params.id)
+      .where('book_id', params.book_id)
+      .firstOrFail()
+    if (await bouncer.with(CommentPolicy).denies('delete', comment)) {
+      return response.unauthorized({
+        messages: "vous n etes pas l'auther de co com",
+      })
+    }
+    await comment.delete()
+
+    return response.noContent()
   }
 }
