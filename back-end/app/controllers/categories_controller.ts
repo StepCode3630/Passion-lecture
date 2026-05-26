218b814fa5f72be58f7dd18484a08398ce00ece8
Stepan <pl77sbr@eduvaud.ch>
2026-05-26 22:23:34 +0200

    chore: menage en supprimant du code mort, services et db.json
    [15] [Done]
    Aussi supprimer update de comment controlleur car inutile

--- back-end/app/controllers/categories_controller.ts
+++ back-end/app/controllers/categories_controller.ts
@@ -28,15 +28,6 @@ export default class CategoriesController {
     return response.ok(categories)
   }
 
-  // /**
-  //  * Handle form submission for the create action
-  //  */
-  // async store({ request }: HttpContext) {
-  //   const data = request.all()
-
-  //   return Category.create(data)
-  // }
-
   /**
    * Show individual record
    */
@@ -44,26 +35,4 @@ export default class CategoriesController {
     const categories = await Category.findOrFail(params.id)
     return categories
   }
-
-  // /**
-  //  * Handle form submission for the edit action
-  //  */
-  // async update({ params, request }: HttpContext) {
-  //   const data = request.all()
-  //   const categories = await Category.findOrFail(params.id)
-
-  //   categories.merge(data)
-  //   await categories.save()
-
-  //   return categories
-  // }
-
-  // /**
-  //  * Delete record
-  //  */
-  // async destroy({ params }: HttpContext) {
-  //   const categories = await Category.findOrFail(params.id)
-  //   await categories.delete()
-  //   return categories
-  // }
 }
